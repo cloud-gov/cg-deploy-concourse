0ee8d8a271907c58c437be874d92a11749089ea6
timothy-spencer <timothy.spencer@gsa.gov>
2018-05-01 14:59:30 -0700

    add in code to let us specify dns servers to the smoke test job

--- ci/smoke-test.sh
+++ ci/smoke-test.sh
@@ -2,6 +2,18 @@
 
 set -eux
 
+# This is here so that the bootstrap concourse can
+# properly smoke test that things are up.  By default,
+# it seems to not have bosh dns working.
+if [ ! -z "${DNS_SERVERS}" ] ; then
+  cp /etc/resolv.conf /tmp/$$.etc.resolv.conf
+  echo "" > /etc/resolv.conf
+  for i in ${DNS_SERVERS} ; do
+    echo "nameserver $i" >> /etc/resolv.conf
+  done
+  cat /tmp/$$.etc.resolv.conf >> /etc/resolv.conf
+  rm /tmp/$$.etc.resolv.conf
+fi
 curl -o fly "${ATC_URL}/api/v1/cli?arch=amd64&platform=linux"
 chmod +x ./fly
 
